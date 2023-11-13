f322ce311f257652f7cfec95c57e507db2284cc0
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-13 09:59:31 +0000

    TitleTextImg component

---
+++ src/TitleTextImg.jsx
@@ -0,0 +1,15 @@
+export default function TitleTextAndImg({title, text, img_source}) {
+    return (
+        <div>
+            <h2>
+                {title}
+            </h2>
+            <p>
+                {text}
+            </p>
+            <div>
+                <img src={img_source}/>
+            </div>`
+        </div>
+    )
+}
