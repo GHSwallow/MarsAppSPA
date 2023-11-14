2358847f8cd25d3d7c7ce2a9fd6315a92ba6071f
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-14 15:39:21 +0000

    new routers for barebones mars app mvp

--- src/main.jsx
+++ src/main.jsx
@@ -7,27 +7,28 @@ import ErrorPage from "./components/error-page.jsx";
 import { SubPage, RootSubpage } from "./routes/subpage.jsx";
 import MarsApp from "./components/MarsApp.jsx";
 import {MarsPATHS, PATHS} from "./routes/paths.jsx";
+import SelectPhotos from "./routes/SelectPhotos.jsx";
 import SelectRover from "./components/SelectRover.jsx";
 createRoutesFromElements()
 const routerTest = createBrowserRouter(
         createRoutesFromElements([
-            <Route path={PATHS.root} element={<Root/>} errorElement={ErrorPage}>
+            <Route path={PATHS.root} element={<Root/>} errorElement={<ErrorPage/>}>
                 <Route path={PATHS.subpage1} element={<RootSubpage/>}/>
             </Route>,
             <Route path={PATHS.subpage2} element={<SubPage/>}/>
             ]
         ))
 
-const router = createBrowserRouter([
-    createRoutesFromElements(
-        <Route path={MarsPATHS.root} element={<MarsRoot/>} errorElement={ErrorPage}>
-            <Route path={MarsPATHS.SelectPhotos} element={<SelectPhotots/>}/>
+const marsRouter = createBrowserRouter(
+    createRoutesFromElements([
+        <Route path={MarsPATHS.root} element={<MarsApp/>} errorElement={<ErrorPage/>}>
+            <Route path={MarsPATHS.SelectPhotos} element={<SelectPhotos/>} errorElement={<ErrorPage/>}/>
         </Route>,
-        )
-])
+        // <Route path={'/'} element={<SelectRover/>} errorElement={<ErrorPage/>}/>
+        ]))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={routerTest} />
+      <RouterProvider router={marsRouter} />
   </React.StrictMode>,
 )
